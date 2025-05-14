7d347a99065d78f44cc28c5a8d70609227b1c29b
Lazism2j <lazism2j@gmail.com>
2025-05-14 14:12:28 +0900

    [Feat] PlayerMovement 구현

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using DesignPattern;
 using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [field: SerializeField][field: Range(0, 10)]
+    public float walkSpeed { get; set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [field: SerializeField][field: Range (0, 10)]
+    public float runSpeed { get; set; }
+
+    [field: SerializeField][field: Range(0, 10)]
+    public float rotateSpeed { get; set; }
+
+    public ObservableProperty<bool> IsAiming { get; private set; } = new();
+    public ObservableProperty<bool> IsMoving { get; private set; } = new();
+    public ObservableProperty<bool> IsAttacking { get; private set; } = new();
 }
